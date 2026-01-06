3928a08c3a77960a35d72963b01c45d2720113a5
louis <louis.absil@gmail.com>
2026-01-06 01:02:56 +0100

    fix: Corriger le base path avec le bon nom de dÃ©pÃ´t

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/opus-lab/', // Chemin de base pour GitHub Pages
+  base: '/Opus-Lab/', // Chemin de base pour GitHub Pages
 })
